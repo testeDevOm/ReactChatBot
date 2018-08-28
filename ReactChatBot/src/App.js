9c54209a8771cfb12391c9433e95233b65f6ee42
Rodnorm <r.normando@hotmail.com>
2018-08-27 22:19:10 -0300

    :alien:
    Some touches

--- ReactChatBot/src/App.js
+++ ReactChatBot/src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
   render() {
     const { feed, sendMessage } = this.props;
     return (
-      <div>
-        <h1>ChatBot says Hi!</h1>
-        <ul>
-          {feed.map( entry => <li>{ entry.text }</li> )}
-        </ul>
-        <input type='text' onKeyDown={ (e) => e.keyCode === 13 ? sendMessage(e.target.value): null} autofocus/>
+      <div class='sidedBox'>
+        <h1>ChatBot</h1>
+          <ul>
+            {feed.map( entry => <li class='fadeOut'>{ entry.text }</li> )}
+          </ul>
+        <input type='text' onKeyDown={ (e) => e.keyCode === 13 ? sendMessage(e.target.value): null} placeholder='Insira sua mensagem...' />
       </div>
     );
   }
